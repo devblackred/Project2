8ec3f11a37291ebf0cbffdf9ad2b69eac382bc84
Celio Munch Junior <129902007+devblackred@users.noreply.github.com>
2024-10-05 20:48:10 -0300

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,25 +1,27 @@
-#!bin/bash
-
+#!/bin/bash
+sudo su
 yum update -y
 yum install -y docker
-yum install -y amazon-efs-utils
-
-systemctl start docker
-systemctl enable docker
-usermod -aG docker ec2-user
-chkconfig docker on
-
-curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+service docker start
+@@ -7,16 +8,17 @@ curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compo
 chmod +x /usr/local/bin/docker-compose
-mv /usr/local/bin/docker-compose /bin/docker-compose
+usermod -aG docker ec2-user
+yum install -y git
+mkdir /mnt/efs
+mkdir /mnt/efs/wordpress
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-XXXXXXXXXXXX.amazonaws.com:/ /mnt/efs
+mkdir /efs
+mkdir /efs/wordpress
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-0e5428e7fc037d824.efs.us-east-1.amazonaws.com:/ efs
 
-curl -sL https://raw.githubusercontent.com/devblackred/Project2/main/docker-compose.yml --output /home/ec2-user/docker-compose.yml
+export WORDPRESS_DB_HOST="XXXXX"
+export WORDPRESS_DB_USER="XXXXX"
+export WORDPRESS_DB_PASSWORD="XXXXX" 
+export WORDPRESS_DB_NAME="XXXXX"
 
-mkdir -p /efs/celio-munch/var/www/html
-
-sudo mount -t efs fs-00551b6438692354b.efs.us-east-1.amazonaws.com:/ efs
-sudo chown ec2-user:ec2-user /efs
-
-echo "fs-00551b6438692354b.efs.us-east-1.amazonaws.com:/ efs nfs defaults 0 0" >> /etc/fstab
-
-docker-compose -f /home/ec2-user/docker-compose.yml up -d
+cd /home/ec2-user
+git clone https://github.com/KerenOlmedo/2_Atividade_PB_Compass.git
+cd 2_Atividade_PB_Compass
+git clone https://github.com/devblackred/Project2.git
+cd Project2
+docker-compose up -d
