5bfc164a10762b14a96d0d844314293a93bf22b3
devblackred <celiomunchjunior4j@gmail.com>
2024-10-05 19:55:28 -0300

    primeiro-commit

---
+++ user-data.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+
+yum update -y
+yum install -y docker
+yum install -y amazon-efs-utils
+
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
+chkconfig docker on
+
+curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+mv /usr/local/bin/docker-compose /bin/docker-compose
+
+curl -sL https://raw.githubusercontent.com/devblackred/Project2-Compass/main/docker-compose.yml --output /home/ec2-user/docker-compose.yml
+
+mkdir -p /efs//var/www/html
+
+sudo mount -t efs fs-00551b6438692354b.efs.us-east-1.amazonaws.com:/ efs
+sudo chown ec2-user:ec2-user /efs
+
+echo "fs-00551b6438692354b.efs.us-east-1.amazonaws.com:/ efs nfs defaults 0 0" >> /etc/fstab
+
+docker-compose -f /home/ec2-user/docker-compose.yml up -d
