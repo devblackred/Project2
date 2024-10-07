fcdeab21ba7fd4d8767f242fce659bc618138235
Celio Munch Junior <129902007+devblackred@users.noreply.github.com>
2024-10-07 19:46:25 -0300

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -5,7 +5,7 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-curl -sL "https://raw.githubusercontent.com/devblackred/Project2-/main/dockercompose.yml" --output "/home/ec2-user/dockercompose.yaml"
+curl -sL "https://raw.githubusercontent.com/devblackred/Project2-CompassUOL/main/docker-compose.yml" --output "/home/ec2-user/docker-compose.yml"
 sudo chmod +x /usr/local/bin/docker-compose
 sudo mv /usr/local/bin/docker-compose /bin/docker-compose
 sudo yum install nfs-utils -y
