4ecd7c20a3b968192efb6f8abc2013400c51fede
Celio Munch Junior <129902007+devblackred@users.noreply.github.com>
2024-10-08 19:54:26 -0300

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -13,6 +13,6 @@ sudo mkdir /mnt/efs/wordpress
 sudo chmod +rwx /mnt/efs/
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-0d6ea7839554ca920.efs.us-east-1.amazonaws.com:/ /mnt/efs
 sudo echo "fs-0d6ea7839554ca920.efs.us-east-1.amazonaws.com:/ /mnt/efs nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0" >> /etc/fstab
-sudo mysql --host="wordpress.ct8i8oe2sycs.us-east-1.rds.amazonaws.com" --user="teste" --password="teste123" --execute="CREATE DATABASE IF NOT EXISTS wordpress;"
+sudo mysql --host="wp.ct8i8oe2sycs.us-east-1.rds.amazonaws.com" --user="teste" --password="teste123" --execute="CREATE DATABASE IF NOT EXISTS wp;"
 sudo cd /home/ec2-user
 sudo docker-compose up -d
